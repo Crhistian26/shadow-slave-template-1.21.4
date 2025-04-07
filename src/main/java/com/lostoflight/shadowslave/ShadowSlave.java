3cd1bfca246749e5f685308c7d3a9313da7d4d67
Crhistian26 <jhan.cterreroramirez26@gmail.com>
2025-04-07 10:25:59 -0400

    Este es el primer commit

--- src/main/java/com/lostoflight/shadowslave/ShadowSlave.java
+++ src/main/java/com/lostoflight/shadowslave/ShadowSlave.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+//Hola
 public class ShadowSlave implements ModInitializer {
 	public static final String MOD_ID = "shadow-slave";
 
